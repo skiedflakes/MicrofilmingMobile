735bfc18a1bbaf089faa4c76604311bf14a891c6
skiedflakes <46420286+skiedflakes@users.noreply.github.com>
2020-09-18 11:35:08 +0800

    jr
    jr

--- src/screen/DeliveriesScreen.js
+++ src/screen/DeliveriesScreen.js
@@ -2,14 +2,11 @@ import * as React from 'react';
 import { StyleSheet,Button, Text, View,TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {createStackNavigator} from '@react-navigation/stack';
-import Deliveries_main from './Abort/Deliveries_main';
+import Deliveries_main from './Deliveries/Deliveries_main';
 const Stack = createStackNavigator();
 export default function DeliveriesScreen() {
-
-   
   return (
     <Stack.Navigator>
-
       <Stack.Screen
         name="Deliveries main"
         component={Deliveries_main}
@@ -17,6 +14,5 @@ export default function DeliveriesScreen() {
       />
     
     </Stack.Navigator>
-    
   );
 }
