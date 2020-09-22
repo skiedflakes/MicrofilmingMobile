2a51801b977edcc6afa8fcf067c4f6aeb68e1ada
skiedflakes <46420286+skiedflakes@users.noreply.github.com>
2020-09-22 21:56:14 +0800

    jr
    jr

--- App.js
+++ App.js
@@ -14,8 +14,14 @@ function App() {
       // global.global_url = 'https://mobile.wdysolutions.com/notes_verifier/main/';
       //global.global_url = 'http://192.168.254.185/PigNotesMobile/';
       // global.global_url = 'http://192.168.8.106/PigNotesMobile_php/';
-       global.global_url = 'http://192.168.41.1/microfilming_localhost/';
-       global.notes_web_directory = 'http://192.168.41.1/notes/';
+
+      //mylocalhost
+      //  global.global_url = 'http://192.168.41.1/microfilming_localhost/';
+      //  global.notes_web_directory = 'http://192.168.41.1/notes/';
+
+      //dev url
+      global.global_url = 'http://mobile.wdysolutions.com/microfilming/main/';
+      global.notes_web_directory = 'https://dev.wdysolutions.com/';
     });
     return (
       <NavigationContainer>
