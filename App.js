5c2c17e3ba3e624cf989fadc0349f0dc63938612
skiedflakes <46420286+skiedflakes@users.noreply.github.com>
2020-09-22 11:40:03 +0800

    jr
    jr

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ function App() {
       //global.global_url = 'http://192.168.254.185/PigNotesMobile/';
       // global.global_url = 'http://192.168.8.106/PigNotesMobile_php/';
        global.global_url = 'http://192.168.41.1/microfilming/';
+       global.notes_web_directory = 'http://192.168.41.1/notes/';
     });
     return (
       <NavigationContainer>
