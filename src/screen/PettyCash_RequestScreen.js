d40103cc86c91e135f72126f718d0b9d411e0bf7
skiedflakes <gagito0594@gmail.com>
2020-10-16 14:53:20 +0800

    jr
    jr

--- src/screen/PettyCash_RequestScreen.js
+++ src/screen/PettyCash_RequestScreen.js
@@ -4,6 +4,7 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import {createStackNavigator} from '@react-navigation/stack';
 import PC_Request_main from './PettyCash/Request/PC_Request_main';
 import Upload_PC_Request from './PettyCash/Request/Upload_PC_Request';
+import Upload_PC_Request_details from './PettyCash/Request/Upload_PC_Request_details';
 import PC_Request_details from './PettyCash/Request/PC_Request_details';
 
 const Stack = createStackNavigator();
@@ -33,7 +34,12 @@ export default function PettyCash_RequetScreen({route}) {
         initialParams={{ company_id: company_id,branch_id: branch_id,company_code: company_code,user_id:user_id }}
         options={{ headerShown: false }}
       />
-    
+          <Stack.Screen
+        name="Upload Request details"
+        component={Upload_PC_Request_details}
+        initialParams={{ company_id: company_id,branch_id: branch_id,company_code: company_code,user_id:user_id }}
+        options={{ headerShown: false }}
+      />
 
     </Stack.Navigator>
   );
