97ddc0a5e14f1ab7587dff7be005fae8e8213ddc
thechosenweak <thechosenweak@gmail.com>
2020-10-19 10:04:36 +0800

    replenish

--- App.js
+++ App.js
@@ -21,8 +21,8 @@ function App() {
     // global.global_url = 'http://192.168.8.106/PigNotesMobile_php/';
 
     //mylocalhost
-    global.global_url = 'http://192.168.2.110/microfilming_localhost/';
-    global.notes_web_directory = 'http://192.168.2.110/notes/';
+    global.global_url = 'http://192.168.2.102/microfilming/';
+    global.notes_web_directory = 'http://192.168.2.102/notes/';
 
     // dev url
     // global.global_url = 'https://mobile.wdysolutions.com/microfilming/main/';
